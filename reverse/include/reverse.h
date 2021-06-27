5f07772e6bcf9036179078f47ec54f1b67a73eaa
fluriafloral <lampia131@gmail.com>
2021-06-26 21:20:57 -0300

    att reverse att reverse

--- reverse/include/reverse.h
+++ reverse/include/reverse.h
@@ -10,13 +10,18 @@ using std::sort;
 
 namespace graal {
 
-/*! 
+/*!
  * TODO: documentação no estilo doxygen
  */
 template<class BidirIt>
 void reverse(BidirIt first, BidirIt last)
 {
-    // TODO
+    BidirIt it1 = first, it2 = last - 1;
+    int d = distance(it1, it2);
+
+    while (it1 < (it1 + (d / 2))){
+        std::iter_swap(it1++, it2--);
+    }
 }
 
 }
